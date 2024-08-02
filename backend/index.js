a6559cd73a7cdd84ba31a403d3bc720285ef3f8a
DainShin <di.shin0611@gmail.com>
2024-08-02 13:33:29 -0400

    first commit

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,20 @@ const app = require("./app");
 const { Server } = require("socket.io");
 var globals = require("./configs/globals");
 
+// 1. create http server
 const httpServer = createServer(app);
 
-// making web socket server and put the httpServer on top of that
+// 2. creating web socket server on top of the http server
 const io = new Server(httpServer, {
     cors: {
         origin: "http://localhost:3000"
     }
 });
 
+// 3. setting web socket server
 require("./utils/io")(io);
 
-// turn on the server
+// 4. turn on the http server
 httpServer.listen(globals.ConnectionString.PORT, () => {
     console.log("Server listening on port", globals.ConnectionString.PORT);
 });
